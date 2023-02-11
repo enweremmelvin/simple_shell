3aadd0c6abb84a007537c44373f1ca7cc644c4d0
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-02-11 14:13:08 -0800

    i don't get those new edge cases code added

--- main.c
+++ main.c
@@ -18,34 +18,30 @@ int main(int argc __attribute__((unused)), char *argv[])
 	wait_time = 2;
 	word_count = 0;
 	command = malloc(sizeof(char) * MAX_INPUT);
-
 	do {
 		if (isatty(0) == 1)
 			printf("($) ");
-
 		user_input = fgets(command, MAX_INPUT, stdin);
 		command = input_parser(command, &word_count);
-		arg = breaker(command, word_count);
+		arg = breaker(command, word_count, environ);
 
 		if (*command == '\0')
+			//probably needed free() here before going back to malloc another 2D array by breaker()
 			continue;
 		if ((arg == NULL) && (isatty(0) == 1))
 		{
 			printf("%s: No such file or directory\n", argv[0]);
+			// we probably needed free here
 			continue;
 		}
 		if ((arg == NULL) && (isatty(0) != 1))
 		{
 			printf("%s: No such file or directory\n", argv[0]);
-			exit(1);
+			exit(1); // need free here
 		}
 		/* checking for malloc */
 		if (user_input == NULL)
-		{
-			free(command);
-			break;
-		}
-
+			break; //doing nothing, have to be deleted
 		fork_id = fork();
 		if (fork_id == 0)
 			exec_ret = execve(arg[0], arg, environ);
